e530839a56619c274d18a497b01ad829480fefc2
Bruno-Ed-cs <brunosilva.pinf@gmail.com>
2025-04-03 20:32:56 -0300

    funciona, arrumar e fazer em arquivo

--- main.py
+++ main.py
@@ -1,6 +1,106 @@
 import numpy as np
+import binarytree as btree
 
-string = "Huffman coding is a lossless data compression algorithm. The idea is to assign variable-length codes to input characters, lengths of the assigned codes are based on the frequencies of corresponding characters. The variable-length codes assigned to input characters are Prefix Codes, means the codes (bit sequences) are assigned in such a way that the code assigned to one character is not the prefix of code assigned to any other character. This is how Huffman Coding makes sure that there is no ambiguity when decoding the generated bitstream. Let us understand prefix codes with a counter example. Let there be four characters a, b, c and d, and their corresponding variable length codes be 00, 01, 0 and 1. This coding leads to ambiguity because code assigned to c is the prefix of codes assigned to a and b. If the compressed bit stream is 0001, the de-compressed output may be “cccd” or “ccb” or “acd” or “ab”."
+string = "entenda meu dilema, espero que este algorítimo funcione" 
+
+
+class NodeHuff(btree.Node):
+    def __init__(self, char=None, weight=None):
+        self.char = char
+        self.value = weight
+        self.left = None
+        self.right = None
+
+def encode(node: NodeHuff, char , sequence = None):
+
+    if sequence is None:
+        sequence = []
+
+    if node.char == char:
+        return sequence.copy()
+    elif node.char != None and node.char != char:
+        return None
+
+
+    if node.left != None:
+        sequence.append(0)
+        seq = encode(node.left, char, sequence)
+        if seq != None:
+            return seq
+        sequence.pop()
+
+    if node.right != None:
+        sequence.append(1)
+        seq = encode(node.right, char, sequence)
+        if seq != None:
+            return seq
+        sequence.pop()
+
+
+    return None
+
+def decode(tree: NodeHuff, code):
+
+    root = tree
+    text = ""
+
+    for bit in code:
+        if bit == 1:
+            if tree.right != None:
+                tree = tree.right
+        else:
+            if tree.left != None:
+                tree = tree.left
+
+        if tree.char != None:
+            text += tree.char
+            tree = root
+
+    return text
+
+
+
+    
+#
+#def bit_iterator(bits):
+#
+#    bit_array = []
+#
+#    while bits > 0:
+#
+#        bit_array.append(bits & 1) 
+#        bits >>= 1
+#
+#    for bit in reversed(bit_array):
+#        yield bit
+#        
+#
+
+def generate_tree(frequency_array):
+
+    node_array = []
+
+    for tup in frequency_array:
+        node_array.append(NodeHuff(tup[0], tup[1]))
+
+
+    while len(node_array) > 1:
+        print(len(node_array))
+        temp = NodeHuff(weight = node_array[0].value + node_array[1].value)
+
+
+        temp.left = node_array.pop(0)
+        temp.right = node_array.pop(0)
+
+        node_array.append(temp)
+
+        node_array.sort(key = lambda node : node.value)
+
+
+    tree : NodeHuff = node_array[0]
+
+    return tree
+
 
 def char_counter(string):
     
@@ -28,5 +128,43 @@ bits = bin(num)
 print(f"Numero original = {num}")
 print(f"Valor em binário = {bits}")
 print(f"histograma = {char_counter(string)}")
+tree = generate_tree(char_counter(string))
+
+print(tree)
+
+print(f"A = {(encode(tree, "a"))}")
+print(f"B = {(encode(tree, "B"))}")
+print(f"C = {(encode(tree, "C"))}")
+print(f"D = {(encode(tree, "D"))}")
+
+encoded = []
+
+for x in string:
+    
+    result = encode(tree, x)
+#    print(result)
+    if result == None:
+        break
+    #print(gap)
+    encoded += result
+
                     
 
+n = np.packbits(encoded)
+print(n)
+print(encoded)
+
+
+u: int = 0
+for num in n:
+    u = u << 8 | num
+
+print(bin(u))
+
+
+
+#test = bin(encode(tree, "D"))
+
+print()
+print(string)
+print(decode(tree, encoded))
